0dd75dca2901ec61805a4a3aa97301017b621d0c
Samuel Haaf <sbhaaf@dons.usfca.edu>
2017-02-20 14:21:01 -0800

    Update project.clj

--- project.clj
+++ project.clj
@@ -1,10 +1,7 @@
 (defproject hayek "0.1.0-SNAPSHOT"
-  :description "FIXME: write description"
-  :url "http://example.com/FIXME"
+  :description "Hayek society of trading bots"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :plugins [[lein-git-deps "0.0.1-SNAPSHOT"]]
-  :git-dependencies [["https://github.com/lunkdjedi/clj-ta-lib"]]
   :dependencies [[org.clojure/clojure "1.8.0"]
                  [clj-ta-lib/clj-ta-lib "0.0.1"]
                  [com.hypirion/clj-xchart "0.2.0"]
